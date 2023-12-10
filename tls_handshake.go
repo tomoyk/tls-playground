30fbf517722afecb7b0b1745c27d6fb48e7325f4
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2023-12-11 00:41:54 +0900

    payload create finished

--- tls_handshake.go
+++ tls_handshake.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 type HandshakeType uint8
@@ -27,20 +28,62 @@ type ProtocolVersion struct {
 
 type Random struct {
 	GmtUnixTime uint32
-	RandomBytes byte[28]
+	RandomBytes [28]byte
 }
-type SessionId byte[32]
+type SessionId [32]byte
+
+type CipherSuite [2]uint8
+
+const (
+	TLS_NULL_WITH_NULL_NULL             CipherSuite = [2]uint8{0x00, 0x00}
+	TLS_RSA_WITH_NULL_MD5               CipherSuite = [2]uint8{0x00, 0x01}
+	TLS_RSA_WITH_NULL_SHA               CipherSuite = [2]uint8{0x00, 0x02}
+	TLS_RSA_WITH_NULL_SHA256            CipherSuite = [2]uint8{0x00, 0x3B}
+	TLS_RSA_WITH_RC4_128_MD5            CipherSuite = [2]uint8{0x00, 0x04}
+	TLS_RSA_WITH_RC4_128_SHA            CipherSuite = [2]uint8{0x00, 0x05}
+	TLS_RSA_WITH_3DES_EDE_CBC_SHA       CipherSuite = [2]uint8{0x00, 0x0A}
+	TLS_RSA_WITH_AES_128_CBC_SHA        CipherSuite = [2]uint8{0x00, 0x2F}
+	TLS_RSA_WITH_AES_256_CBC_SHA        CipherSuite = [2]uint8{0x00, 0x35}
+	TLS_RSA_WITH_AES_128_CBC_SHA256     CipherSuite = [2]uint8{0x00, 0x3C}
+	TLS_RSA_WITH_AES_256_CBC_SHA256     CipherSuite = [2]uint8{0x00, 0x3D}
+	TLS_DH_DSS_WITH_3DES_EDE_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x0D}
+	TLS_DH_RSA_WITH_3DES_EDE_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x10}
+	TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA   CipherSuite = [2]uint8{0x00, 0x13}
+	TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA   CipherSuite = [2]uint8{0x00, 0x16}
+	TLS_DH_DSS_WITH_AES_128_CBC_SHA     CipherSuite = [2]uint8{0x00, 0x30}
+	TLS_DH_RSA_WITH_AES_128_CBC_SHA     CipherSuite = [2]uint8{0x00, 0x31}
+	TLS_DHE_DSS_WITH_AES_128_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x32}
+	TLS_DHE_RSA_WITH_AES_128_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x33}
+	TLS_DH_DSS_WITH_AES_256_CBC_SHA     CipherSuite = [2]uint8{0x00, 0x36}
+	TLS_DH_RSA_WITH_AES_256_CBC_SHA     CipherSuite = [2]uint8{0x00, 0x37}
+	TLS_DHE_DSS_WITH_AES_256_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x38}
+	TLS_DHE_RSA_WITH_AES_256_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x39}
+	TLS_DH_DSS_WITH_AES_128_CBC_SHA256  CipherSuite = [2]uint8{0x00, 0x3E}
+	TLS_DH_RSA_WITH_AES_128_CBC_SHA256  CipherSuite = [2]uint8{0x00, 0x3F}
+	TLS_DHE_DSS_WITH_AES_128_CBC_SHA256 CipherSuite = [2]uint8{0x00, 0x40}
+	TLS_DHE_RSA_WITH_AES_128_CBC_SHA256 CipherSuite = [2]uint8{0x00, 0x67}
+	TLS_DH_DSS_WITH_AES_256_CBC_SHA256  CipherSuite = [2]uint8{0x00, 0x68}
+	TLS_DH_RSA_WITH_AES_256_CBC_SHA256  CipherSuite = [2]uint8{0x00, 0x69}
+	TLS_DHE_DSS_WITH_AES_256_CBC_SHA256 CipherSuite = [2]uint8{0x00, 0x6A}
+	TLS_DHE_RSA_WITH_AES_256_CBC_SHA256 CipherSuite = [2]uint8{0x00, 0x6B}
+	TLS_DH_anon_WITH_RC4_128_MD5        CipherSuite = [2]uint8{0x00, 0x18}
+	TLS_DH_anon_WITH_3DES_EDE_CBC_SHA   CipherSuite = [2]uint8{0x00, 0x1B}
+	TLS_DH_anon_WITH_AES_128_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x34}
+	TLS_DH_anon_WITH_AES_256_CBC_SHA    CipherSuite = [2]uint8{0x00, 0x3A}
+	TLS_DH_anon_WITH_AES_128_CBC_SHA256 CipherSuite = [2]uint8{0x00, 0x6C}
+	TLS_DH_anon_WITH_AES_256_CBC_SHA256 CipherSuite = [2]uint8{0x00, 0x6D}
+)
 
 type ClientHello struct {
 	ClientVersion      ProtocolVersion
 	Random             Random
 	SessionId          SessionId
-	CipherSuites       uint16
+	CipherSuites       []CipherSuite
 	CompressionMethods uint8
 	Extensions         uint16
 }
 
-type Handshake struct {
+type HandshakeProtocol struct {
 	MsgType HandshakeType
 	Length  uint32 // uint24
 	Body    ClientHello
@@ -48,6 +91,31 @@ type Handshake struct {
 
 func main() {
 	log.Println(Finished)
+
+	// timestamp
+	dt := time.Now()
+	log.Println(dt)
+	unix := dt.Unix()
+	log.Println(unix)
+
+	h := HandshakeProtocol{
+		MsgType: ClientHello,
+		Length:  1,
+		Body: ClientHello{
+			ClientVersion: ProtocolVersion{
+				Major: 3,
+				Minor: 3,
+			},
+			Random: Random{
+				GmtUnixTime: unix,
+				RandomBytes: []byte("abcdefghijklmnopqrstuvwxyz12"),
+			},
+			SessionId:          []byte(""),
+			CipherSuites:       []CipherSuites{TLS_RSA_WITH_RC4_128_MD5},
+			CompressionMethods: 0,
+			Extensions:         0,
+		},
+	}
 	// log.Println("Connecting ...")
 	// // サーバーに接続
 	// conn, err := net.Dial("tcp", "1.1.1.1:443")
